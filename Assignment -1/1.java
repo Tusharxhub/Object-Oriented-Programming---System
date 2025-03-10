8e8773479685ddc8ce49b0d836b910e9058c4630
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-03-10 18:16:21 +0530

    first commit

---
+++ Assignment -1/1.java
@@ -0,0 +1,10 @@
+//!  rite a Java program using a class. The name of the class is `MyClass` that consists of a single method. Print your name, department, and roll number using that single method.
+
+
+class MyClass {
+    public void printDetails() {
+        System.out.println("Name: Tushar");
+        System.out.println("Department: Computer Science");
+        System.out.println("Roll Number: 029");
+    }
+}
