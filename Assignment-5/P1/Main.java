8684443f83b36389304373e36a9ccad437d602bd
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-03-11 14:27:06 +0530

    Add P1 package with Main class and main method to print 'Tushar'

---
+++ Assignment-5/P1/Main.java
@@ -0,0 +1,17 @@
+//! Write a Java program to create a package with a single class named 'P1' and a main method in the class.
+
+package P1;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Tushar");
+    }
+}
+
+//? The package name is 'P1' and the class name is 'Main'. The main method prints 'Tushar' to the console.
+
+// ? to compile the program, use the following command:
+//? javac P1/Main.java
+// ? java P1.Main
