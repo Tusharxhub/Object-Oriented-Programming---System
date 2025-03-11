7d26c7613b78b5e773ea36428f17a0f2266755a2
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-03-11 18:59:05 +0530

    Refactor series classes to implement Runnable interface and streamline series calculation in main method

--- Assignment-7/mainX.java
+++ Assignment-7/mainX.java
@@ -1,58 +1,46 @@
-//! Wap in Java to define two classes which are implementing an interface. Print the sum of the numeric series in the given method of the class. Create objects of both the classes in main method and display() the respective series using threads.
+//! Write a prog in java to define two classes which implements runnable interface. Print the sum series in the run method of AP class and print the multiple series in the run method of GP class. Create objects of both the classes in main method and display the series.
 
-interface Series {
-    void display();
-}
-
-class ArithmeticSeries implements Series, Runnable {
+class AP implements Runnable {
     int n;
 
-    ArithmeticSeries(int n) {
+    AP(int n) {
         this.n = n;
     }
 
-    public void display() {
+    public void run() {
         int sum = 0;
         for (int i = 1; i <= n; i++) {
             sum += i;
         }
         System.out.println("Sum of Arithmetic series up to " + n + " terms: " + sum);
     }
-
-    public void run() {
-        display();
-    }
 }
 
-class GeometricSeries implements Series, Runnable {
+class GP implements Runnable {
     int n;
     int r;
 
-    GeometricSeries(int n, int r) {
+    GP(int n, int r) {
         this.n = n;
         this.r = r;
     }
 
-    public void display() {
+    public void run() {
         int sum = 0;
         for (int i = 0; i < n; i++) {
             sum += Math.pow(r, i);
         }
         System.out.println("Sum of Geometric series up to " + n + " terms: " + sum);
     }
-
-    public void run() {
-        display();
-    }
 }
 
 public class mainX {
     public static void main(String[] args) {
-        ArithmeticSeries as = new ArithmeticSeries(5);
-        GeometricSeries gs = new GeometricSeries(5, 2);
+        AP ap = new AP(5);
+        GP gp = new GP(5, 2);
 
-        Thread t1 = new Thread(as);
-        Thread t2 = new Thread(gs);
+        Thread t1 = new Thread(ap);
+        Thread t2 = new Thread(gp);
 
         t1.start();
         t2.start();
