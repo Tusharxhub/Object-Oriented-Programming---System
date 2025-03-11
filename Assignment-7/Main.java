eaefbe8ee6ffe1e82e844bd927c6bf367c351003
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-03-11 18:08:59 +0530

    Add multithreading implementation with FirstThread and SecondThread classes to execute concurrent loops

---
+++ Assignment-7/Main.java
@@ -0,0 +1,41 @@
+//! Wap in JAVA to implement two threads named 1st thread and 2nd thread in each class. Initialize the variable with constructor. Override method to execute a loop from 0 to 10.
+
+class FirstThread extends Thread {
+    FirstThread() {
+        super("First Thread");
+        System.out.println("First Thread Created");
+    }
+
+    public void run() {
+        System.out.println("First Thread starts:");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("First Thread: " + i);
+        }
+        System.out.println("First Thread ends");
+    }
+}
+
+class SecondThread extends Thread {
+    SecondThread() {
+        super("Second Thread");
+        System.out.println("Second Thread Created");
+    }
+
+    public void run() {
+        System.out.println("Second Thread starts:");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Second Thread: " + i);
+        }
+        System.out.println("Second Thread ends");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        FirstThread ft = new FirstThread();
+        SecondThread st = new SecondThread();
+        
+        ft.start();
+        st.start();
+    }
+}
