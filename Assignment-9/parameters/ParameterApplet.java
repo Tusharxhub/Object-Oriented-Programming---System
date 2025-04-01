e89b29a6655409f67a28d6f51e5f7a77334bc7ad
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-04-01 21:51:31 +0530

    Refactor ParameterApplet to use a single message parameter and update HTML for clarity

--- Assignment-9/parameters/ParameterApplet.java
+++ Assignment-9/parameters/ParameterApplet.java
@@ -4,28 +4,16 @@ import java.applet.Applet;
 import java.awt.Graphics;
 
 public class ParameterApplet extends Applet {
-    private String param1;
-    private String param2;
+    String message;
 
-    @Override
     public void init() {
-        param1 = getParameter("param1");
-        param2 = getParameter("param2");
-
-        if (param1 == null) {
-            param1 = "Default Value 1";
-        }
-        if (param2 == null) {
-            param2 = "Default Value 2";
+        message = getParameter("message");
+        if (message == null) {
+            message = "No parameter found!";
         }
     }
 
-    @Override
     public void paint(Graphics g) {
-        g.drawString("Parameter 1: " + param1, 20, 50);
-        g.drawString("Parameter 2: " + param2, 20, 70);
+        g.drawString("Message from HTML: " + message, 20, 50);
     }
 }
-
-
-
