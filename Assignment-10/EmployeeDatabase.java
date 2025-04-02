ce299e1653b4b6c618d4dfe90551a7859bf0d686
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-04-02 19:37:47 +0530

    Add detailed comments to EmployeeDatabase.java for database setup and sample data insertion

--- Assignment-10/EmployeeDatabase.java
+++ Assignment-10/EmployeeDatabase.java
@@ -35,6 +35,37 @@ public class EmployeeDatabase {
 
 
 
+//? First you need to make a database and a table named "employees" in your MySQL server. In my case I have created a database named "tushar" and a table named "employees".
+//? The table "employees" should have the following structure:
+//? CREATE TABLE employees (
+//?     id INT AUTO_INCREMENT PRIMARY KEY,
+//?     name VARCHAR(100) NOT NULL,
+//?     position VARCHAR(100) NOT NULL,
+//?     salary DECIMAL(10, 2) NOT NULL
+//? );
+//? You can insert some sample data into the table using the following SQL commands:
+
+
+
+//? INSERT INTO employees (name, position, salary) VALUES
+//? ('Tushar kanti Dey', 'Software Engineer', 60000.00),
+//? ('Abhishek Singh', 'Project Manager', 75000.00),
+//? ('Aditya Dey', 'HR Manager', 65000.00);
+//? ('Baibhab Adhikari', 'Web dev', 80000.00);
+//? ('Shovon Halder', 'UI/UX Designer', 50000.00);
+
+
+
+//? Then tou get the following output:
+//? Name | Position | Salary
+//? Tushar kanti Dey | Software Engineer | 60000.00
+//? Abhishek Singh | Project Manager | 75000.00
+//? Aditya Dey | HR Manager | 65000.00
+//? Baibhab Adhikari | Web dev | 80000.00
+//? Shovon Halder | UI/UX Designer | 50000.00
+
+
+
 
 //? This program connects to a MySQL database named "tushar" and retrieves employee records from the "employees" table.
 //? It uses JDBC to establish the connection, execute a SQL query, and display the results in a formatted manner.
@@ -51,5 +82,5 @@ public class EmployeeDatabase {
 //? To run the program, following commands can be used in the terminal:
 //? 1. Compile the program: javac EmployeeDatabase.java
 //? 2. Run the program: java -cp .:mysql-connector-java-x.x.x.jar EmployeeDatabase
-//? (Replace x.x.x with the version of the MySQL connector you are using.)   [In my case my version is mysql-connector-j-9.2.0]
+//? (Replace x.x.x with the version of the MySQL connector you are using.)   [In my case my version is mysql-connector-j-9.2.0 so I will replace mysql-connector-java-x.x.x.jar with mysql-connector-j-9.2.0.jar]
 //? Make sure to have the MySQL server running and the database and table created before executing the program.
